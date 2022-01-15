478e297342b4f7e92622807c5feac47d1eae7f04
choi2601 <byeonghyeonchoe55@gmail.com>
2022-01-15 16:36:30 +0900

    Modify: MenuHome 반응형 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -214,6 +214,14 @@ const MenuList = styled.li`
     @media (min-width: 768px) and (max-width: 991px) {
       display: none;
     }
+
+    @media (min-width: 992px) and (max-width: 1100px) {
+      display: none;
+    }
+
+    @media (min-width: 1100px) {
+      display: none;
+    }
   }
 
   &.selectNav {
